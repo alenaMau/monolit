112bd7e0d244cbc1bc64d0c2e64b06fa3034ba27
AlenaMau <alenamau3486@gmail.com>
2023-12-21 21:55:03 +0700

    monolit доделала

---
+++ mysite/polls/urls.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('results/<int:pk>/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('registration/', views.regView, name='reg'),
+    path('registration/login', views.MyLoginView.as_view(), name='login'),
+    path('accounts/<int:pk>', views.Profile.as_view(), name='profile'),
+    path('delete_profile/<int:pk>', views.delete_profile, name='del_prof'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('history_list/', views.HistoryList.as_view(), name='history_list'),
+]
+
